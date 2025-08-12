9b21c1f81e0c512391676bfe7a8a72e6804c7aec
Dillo <espressobot@gmail.com>
2025-08-11 17:29:14 -0700

    These scripts wrap the Docker 'run' commands for running the basic NOVA Carter demos. They're super-simple, could be expanded/coalesced but already save a lot of cut & paste.

---
+++ opennav_visual_nav_demo/tools/run_docker_lidar_mapping.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+docker run --privileged --network host \
+    -v /dev/*:/dev/* \
+    -v /tmp/argus_socket:/tmp/argus_socket \
+    -v /etc/nova:/etc/nova \
+    nvcr.io/nvidia/isaac/nova_carter_bringup:release_3.2-aarch64 \
+    ros2 launch nova_carter_bringup lidar_mapping.launch.py
+
---
+++ opennav_visual_nav_demo/tools/run_docker_nova_carter_shell.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker run -it --privileged --network host \
+    -v /dev/*:/dev/* \
+    -v /tmp/argus_socket:/tmp/argus_socket \
+    -v /etc/nova:/etc/nova \
+    nvcr.io/nvidia/isaac/nova_carter_bringup:release_3.2-aarch64 \
+    /bin/bash
